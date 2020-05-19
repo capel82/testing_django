a3a86b56f1b1e40213b7c287d6b732164d7662b0
capel82 <sandoralai@gmail.com>
2020-05-19 10:33:33 +0000

    created static files and initial templates testing

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
-def index(response):
-        return HttpResponse('<h1>Hello World')
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+    
