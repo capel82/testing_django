0880565d1c754039040293944b5d5766fa0c8839
capel82 <sandoralai@gmail.com>
2020-05-26 15:19:50 +0000

    customised admin course table in admin models.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from .models import Course
-# Register your models here.
-admin.site.register(Course)
+# models for admin table so the id and title are linked, filter category, editable options and ordering datetime.
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category','weekday_datetime','weekends_datetime', 'is_published', 'is_next_event')
+    list_display_links = ('id', 'title')
+    list_filter = ('category',)
+    list_editable = ('is_published', 'is_next_event')
+    list_per_page = 20
+    ordering = ('weekday_datetime',)
+
+admin.site.register(Course, CourseAdmin)
